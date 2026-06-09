05c12ad823356c56f8eab7d6e34513b2388ee4dd
CASSEL Kadir <12402678@r20517.iutv.univ-paris13.fr>
2026-06-09 16:44:32 +0200

    Ajout des tests Mockito pour comprendre coment ça fonctionne

--- src/test/java/com/example/demo/service/StatistiqueTests.java
+++ src/test/java/com/example/demo/service/StatistiqueTests.java
@@ -5,13 +5,29 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import org.mockito.Mockito;
+
 import static org.mockito.Mockito.*;
 
-
 @SpringBootTest
-public class StatistiqueTests {
+class StatistiqueTests {
 
-    @MockBean
-    StatistiqueImpl statistiqueImpl;
+	@MockBean
+	StatistiqueImpl statistiqueImpl;
 
+	@Test
+	public void avecMockito() throws Exception {
+        Voiture v1 = new Voiture("Tesla", 2000);
+        Voiture v2 = new Voiture("Toyota", 4000);
+
+		StatistiqueImpl resultat = Mockito.mock(StatistiqueImpl.class);
+		// 1 argument, returns void
+		doNothing().when(resultat).ajouter(v1);
+        // 1 argument, returns void
+		doNothing().when(resultat).ajouter(v2);
+		// 0 argument, throws exception
+		doThrow(Exception.class).when(resultat).ajouter(null);
+		// 1 argument, return void
+		doNothing().when(resultat).prixMoyen();
+	}
 }
